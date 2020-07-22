70ae46ff4fb3b3c63e49e79ccf80a91a41081eec
akrabatt <Starwars-1999@yandex.ru>
2020-07-22 23:31:21 +0200

    new file

--- parser_MPLS.py
+++ parser_MPLS.py
@@ -3,7 +3,23 @@ from bs4 import BeautifulSoup
 import xlrd
 import xlwt
 
-URL = 'https://www.megapolys.com/catalog/stroitelnye_materialy/'
+URL = [
+    # 'https://www.megapolys.com/catalog/stroitelnye_materialy/',
+    # 'https://www.megapolys.com/catalog/tovary_dlya_dachi_i_sada/',
+    # 'https://www.megapolys.com/catalog/elektrooborudovanie/',
+    # 'https://www.megapolys.com/catalog/otoplenie_i_vodosnabzhenie/',
+    # 'https://www.megapolys.com/catalog/napolnye_pokrytiya/',
+    # 'https://www.megapolys.com/catalog/tovary_dlya_doma_i_byta/',
+    # 'https://www.megapolys.com/catalog/dekorativno_otdelochnye_materialy/',
+    # 'https://www.megapolys.com/catalog/stolyarnye_izdeliya/',
+    # 'https://www.megapolys.com/catalog/santekhnika/',
+    # 'https://www.megapolys.com/catalog/instrumenty_tekhnika/',
+    # 'https://www.megapolys.com/catalog/avtotovary/',
+    # 'https://www.megapolys.com/catalog/osveshchenie/',
+    # 'https://www.megapolys.com/catalog/plitka/',
+    'https://www.megapolys.com/catalog/tekhnicheskie_sredstva_bezopasnosti/',
+    # 'https://www.megapolys.com/catalog/tekhno_siti_1/'
+]
 
 HEADERS = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
@@ -14,6 +30,10 @@ HEADERS = {
 
 FILE = ''
 HOST = 'https://www.megapolys.com'
+
+book = xlrd.open_workbook('./eq_EC.csv.xls', formatting_info=True)
+sheet = book.sheet_by_index(0)
+sheet1 = xlwt.Workbook.sheet()
 
 equipment = []
 equipment_1 = []
@@ -28,38 +48,71 @@ def get_html(url, params=None):
 
 '''получаем количество страниц'''
 def get_pages_count(html):
-    pass
+    soup = BeautifulSoup(html, 'html.parser')
+    pagination = soup.find_all('a', class_='dark_link')
+    if pagination:
+        return int(pagination[-1].get_text())
+    else:
+        return 1
 
 
 '''получаем контент ='''
 def get_content(html, equipment):
+
     soup = BeautifulSoup(html, 'html.parser')
     items = soup.find_all('div', class_= 'item_block col-4 col-md-3 col-sm-6 col-xs-6')
     for item in items:
-        link = HOST + item.find('a', class_='dark_link').get('href')
-        title_value = item.find('div', {'class': 'item-title'}).get_text(strip=True)  # используем другой способ поиска
-        price_value = item.find('span', class_='price_value').get_text(strip=True)
+        try:
+            link = HOST + item.find('a', class_='dark_link').get('href')
+            title_value = item.find('div', {'class': 'item-title'}).get_text(strip=True)  # используем другой способ поиска
+            price_value = item.find('span', class_='price_value').get_text(strip=True)
 
-        equipment.append({
-            'title': title_value,
-            'price': price_value,
-            'link': link
-        })
-    print(equipment)
+            equipment.append({
+                'title': title_value,
+                'price': price_value,
+                'link': link
+            })
+        except AttributeError:
+            price_value = 'no price'
+            equipment.append({
+                'title': title_value,
+                'price': price_value,
+                'link': link
+            })
+    return equipment
 
 
 '''сохраняем В файл'''
-def save_in_file():
-    pass
+def save_in_file(eq):
+    a = 0
+    for i in range(len(equipment)):
+        title = equipment[i]['title']
+        price = equipment[i]['price']
+        link = equipment[i]['link']
+        xlwt.sheet1.write(a, 4, title)
+        xlwt.sheet1.write(a, 5, price)
+        xlwt.sheet1.write(a, 6, link)
+        a += 1
+
+
 
 '''основная функция'''
 def parse():
-    html = get_html(URL)
-    if html.status_code == 200:
-        print('Доступ к html. Статус: Успешно')
-        get_content(html.text, equipment)
-    else:
-        print('Доступ к html. Статус: Ошибка')
-
+    for u in range(len(URL)):
+        html = get_html(URL[u])
+        if html.status_code == 200:
+            print('Доступ к html. Статус: Успешно')
+            pages_count = get_pages_count(html.text)
+            for page in range(1, pages_count + 1):
+                print(f'Парсинг страницы {page} из {pages_count}...')
+                html = get_html(URL[u], params={'PAGEN_1': page})
+                equipment_1.extend(get_content(html.text, equipment))
+            # print(equipment)
+            print(len(equipment))
+        else:
+            print('Доступ к html. Статус: Ошибка')
+    save_in_file(equipment)
+    book.save()
+    print('файл сохранён')
 
 parse()
