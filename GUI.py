c4cccc54e4e0a97da570e168bb377d4bfae2d892
akrabatt <Starwars-1999@yandex.ru>
2020-08-05 23:26:18 +0200

    new file

--- GUI.py
+++ GUI.py
@@ -1,69 +1,31 @@
-# import xlwt, xlrd
-# import sys
-# from PyQt5.QtWidgets import QApplication, QWidget
-#
-# work_book1 = xlrd.open_workbook('./eq_EC.xls.xls', formatting_info=True)
-# work_book2 = xlrd.open_workbook('./eq_MPLS.xls', formatting_info=True)
-#
-# sheet1 = work_book1.sheet_by_index(0)
-# sheet2 = work_book2.sheet_by_index(0)
-#
-# book_for_write = xlwt.Workbook('utf8')  # создаём книгу
-# sheet_for_write = book_for_write.add_sheet('EC_MPLS')  # создаём лист в этой книге
-#
-# vals1 = [sheet1.row_values(rownum) for rownum in range(sheet1.nrows)]
-# vals2 = [sheet2.row_values(rownum) for rownum in range(sheet2.nrows)]
-#
-# # Эл. счетчик Нева 303, 5-100A, 220*380В, МОУ (в упак. 30...
-#
-# search = 'Эл. счетчик Нева 303, 5-100A, 220*380В, МОУ (в упак. 30...'
-#
-# for i in range(len(vals1)):
-#     if search in vals1[i][0]:
-#         print(vals1[i])
-#         print(i)
-#
-# print(vals1[19])
-
 import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from XLSGUI import Ui_Dialog
 
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import (QMainWindow, QApplication, QWidget,
-                             QPushButton, QAction, QLineEdit, QMessageBox)
+equipment = []
 
+# create app
+app = QtWidgets.QApplication(sys.argv)
 
-class App(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.title = 'Тест'
-        self.left = 200
-        self.top = 200
-        self.width = 400
-        self.height = 140
-        self.initUI()
+# init
+Dialog = QtWidgets.QDialog()
+ui = Ui_Dialog()
+ui.setupUi(Dialog)
+Dialog.show()
 
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-
-        self.textbox = QLineEdit(self)
-        self.textbox.move(20, 20)
-        self.textbox.resize(280, 40)
-
-        self.button = QPushButton('Показать', self)
-        self.button.move(20, 80)
-
-        self.button.clicked.connect(self.on_click)
-        self.show()
-
-    # @pyqtSlot()
-    def on_click(self):
-        textboxValue = self.textbox.text()
-        QMessageBox.question(self, 'Введено', textboxValue, QMessageBox.Ok, QMessageBox.Ok)
-        self.textbox.setText("")
+# logic
+def get_equipment ():
+    eq = ui.lineEdit.text()
+    equipment.append(eq)
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    sys.exit(app.exec_())
+def save_list():
+    pass
+
+
+ui.pushButton.clicked.connect(get_equipment)
+ui.pushButton_2.clicked.connect(get_equipment)
+
+# main loop
+sys.exit(app.exec_())
+
---
+++ XLSGUI.py
@@ -0,0 +1,37 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(565, 268)
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(230, 180, 91, 41))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2.setGeometry(QtCore.QRect(350, 180, 91, 41))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(40, 40, 451, 31))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(40, 150, 331, 31))
+        self.label_2.setObjectName("label_2")
+        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit.setGeometry(QtCore.QRect(40, 80, 471, 31))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_2.setGeometry(QtCore.QRect(40, 190, 113, 31))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.pushButton.setText(_translate("Dialog", "Добавить"))
+        self.pushButton_2.setText(_translate("Dialog", "Закончить"))
+        self.label.setText(_translate("Dialog", "Введите название оборудования"))
+        self.label_2.setText(_translate("Dialog", "Введите количество"))
+
